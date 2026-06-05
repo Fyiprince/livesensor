f8da071ae8b72991b80bb0531c8e1a04ce3c237e
prince <hereisyourbatman@gmail.com>
2026-06-05 11:14:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'sensor',
+    version = '0.0.1',
+    author = 'prince',
+    author_email = 'princenitrov15@gamil.com',
+    packages = find_packages(),
+)
